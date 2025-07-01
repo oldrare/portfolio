43f42c808e93de4723616e66f6a6cf3d5bf9dfde
oldrare <oldrare112@gmail.com>
2025-07-01 18:01:12 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// 다크 모드 toggle
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
+
+// scroll reveal
+window.addEventListener('scroll', function() {
+    var reveals = document.querySelectorAll('.reveal');
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 100;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add('show');
+        }
+    }
+});
