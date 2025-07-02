da3c249fa22aa678bab708874ff238296c92ddd9
oldrare <oldrare112@gmail.com>
2025-07-02 14:18:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,37 @@ window.onload = function() {
         options:{ plugins:{legend:{display:false}}, scales:{x:{display:false}, y:{display:false}} }
     });
 };
+
+
+window.onload = function() {
+    const ctx = document.getElementById('salesCompareChart').getContext('2d');
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['4월', '5월', '6월'],
+            datasets: [
+                {
+                    label: '2024',
+                    data: [9100000, 10100000, 9500000],
+                    backgroundColor: 'rgba(54, 162, 235, 0.7)'
+                },
+                {
+                    label: '2025',
+                    data: [16100000, 16200000, 15900000],
+                    backgroundColor: 'rgba(255, 99, 132, 0.7)'
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        callback: value => value.toLocaleString() + '₩'
+                    }
+                }
+            }
+        }
+    });
+};
